cfcba7042144679a0305b4e8e56dcff7a0c0aaae
Ishmael Sweeney <ishmaelsweeney@campus-020-018.ucdavis.edu>
2024-10-03 14:00:04 -0700

    week2

---
+++ week2_introtoR.R
@@ -0,0 +1,46 @@
+3+3
+2*5
+#orderofoeprations
+4+(8*3)^2
+#scientific notation
+2/100000
+4e3
+#mathematical functions
+exp(3)
+log(4)
+sqrt(4)
+
+?log
+log(2,4)
+log(4,2)
+log(x=4, base=2)
+x<- 1
+x
+rm(x)
+#nestedfunctions
+sqrt(exp(4))
+log(exp(100))
+mynumber <- 6
+mynumber==5
+mynumber != 5
+mynumber > 4
+mynumber>=3
+mynumber<=4
+mynumber <- 7
+othernumber <- -3
+mynumber * othernumber
+#object name conventions
+numSamples <- 50
+num_samples <- 40
+rm(num_samples)
+log_of_word <- log("a_word")
+log_of_word
+log_of_negative <- log(-2)
+log_of_negative
+#challenge
+elephant1_kg <- 3492
+elephant2_lb <- 7757
+elephant1_lb <- elephant1_kg * 2.2
+elephant2_lb > elephant1_lb
+myelephants <- c(elephant1_lb, elephant2_lb)
+which(myelephants == max(myelephants))
