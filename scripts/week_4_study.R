e6274a663c4849c24d0aa6c46906d12899201a86
Ishmael Sweeney <ishmaelsweeney@Ishmaels-MacBook-Pro-2.local>
2024-10-17 15:03:10 -0700

    as

--- scripts/week_4_study.R
+++ scripts/week_4_study.R
@@ -6,20 +6,31 @@ surveys <- read_csv("data/portal_data_joined.csv")
 
 str(surveys) #shows dataset of surveys
 
-#select colums
+#select columns, select certain columns
 
 month_day_year <- select(surveys, month, day, year)
 
-#filtering by equals
+#filtering by equals, to select certain rows
 
 filter(surveys, year == 1981)
+year_1981 <- filter(surveys, year == 1981)
+  ## year_1981_baser <- surveys[surveys$year == 1981, ] is baser version
+
+sum(year_1981$year != 1981, na.rm = T)
+
 
 #filtering by range (ex. multiple years) 
+#filter r base
+the80s <- surveys[surveys$year %in% 1981:1983, ]
+#filter tidy
+
+the80stidy <- filter(surveys, year %in% 1981:1983)
+
 filter(surveys, year %in% c(1981:1983))
     # %in% does bucket matching, looks at years found in bucket of 1981-1983
 
 #review: why you should NEVER do: 
-filter(surveys, year == c(1981, 1982, 1983))
+the80srecycle <- filter(surveys, year == c(1981, 1982, 1983))
     # This recycles the vector.
     # c(1981, 1982, 1983) only checks each subsequent row (1,2,3) to see if
     # it matches with the given sequence, not properly filtering out years
@@ -29,7 +40,7 @@ bigfoot_with_weight <- filter(surveys, hindfoot_length > 40 & !is.na(weight))
 
 #multi-step process
 small_animals <- filter(surveys, weight < 5)
-    #this is dangerous, because you must remeber to select from 'small_animals' dataframe just created and NOT surveys
+    #this is dangerous, because you must remeber to select from 'small_animals' data frame just created and NOT surveys
 
 small_animals_id <- select(small_animals, record_id, plot_id, species_id)
 
@@ -38,13 +49,13 @@ small_animals_id <- select(small_animals, record_id, plot_id, species_id)
 small_animals_id <- select(filter(surveys, weight < 5), record_id, plot_id, species_id)
 
 #same process can be done using a pipe
-#Cmd shift M 
+#Cmd shift M (%>%)
 # or |>
 
 small_animals_id <- filter(surveys, weight < 5) %>%  select(record_id, plot_id, species_id)
 
 #same as
-small_animals_id <- surveys %>%  filter(weight < 5) %>% select(record_id, plot_id,species_id)
+small_animals_id <- surveys %>%  filter(., weight < 5) %>% select(.,record_id, plot_id,species_id)
 
 #how to do line breaks with pipes
 surveys %>% filter(month==1)
@@ -60,3 +71,16 @@ surveys
 #line break rules: after open parenthesis, pipe, commas, or anything that shows the line is NOT complete yet
   #cant end statement on a pipe
   
+#one final review of a concept
+
+mini <- surveys[190:209, ]
+table(mini$species_id)
+
+#rows
+nrow(mini)
+
+test <- mini %>% filter(species_id %in% c("DM","NL"))
+nrow(test)
+
+
+### PART 2
